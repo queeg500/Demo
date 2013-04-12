3ec958d3bacaca829ea697b97296903fa0c70da4
Dan <Dan@holofernes-xp.home>
2013-04-12 11:25:26 +0100

    Merged and added class body

--- Demo/src/anotherPackage/ClassA.java
+++ Demo/src/anotherPackage/ClassA.java
@@ -2,5 +2,16 @@ package anotherPackage;
 
 public class ClassA
 {
-
+   private final int i;
+   
+   public ClassA( int i )
+   {
+      this.i = i;
+   }
+   
+   @Override
+   public String toString()
+   {
+      return Integer.toString( i );
+   }
 }
